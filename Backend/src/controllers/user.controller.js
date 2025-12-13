12b032c0d0a704344ab591c8aa11b88e779dcb95
istekhar <istekhararkam25@gmail.com>
2025-12-13 19:39:59 +0530

    feat: add routes and controllers for comments, likes, playlists, subscriptions, tweets, and videos; implement healthcheck endpoint

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -4,7 +4,7 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt, { decode } from "jsonwebtoken";
-
+import mongoose from "mongoose";
 // here to generate token first find the user and then call the method to generate token from model
 
 const generateAccessAndRefreshToken = async (userId) => {
@@ -177,8 +177,8 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined,
+      $unset: {
+        refreshToken: 1,
       },
     },
     { new: true }
