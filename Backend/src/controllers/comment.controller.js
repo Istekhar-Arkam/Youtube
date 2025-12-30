52fb77285efeecb9bbeef8f0daaf4fd1e74566d7
istekhar <istekhararkam25@gmail.com>
2025-12-30 12:12:23 +0530

    feat: implement error handling and response structure for comment update and deletion, enhance healthcheck response

--- Backend/src/controllers/comment.controller.js
+++ Backend/src/controllers/comment.controller.js
@@ -1,9 +1,10 @@
-import mongoose, { isValidObjectId } from "mongoose";
+import mongoose from "mongoose";
 import { Comment } from "../models/comment.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Video } from "../models/video.model.js";
+
 const getVideoComments = asyncHandler(async (req, res) => {
   //TODO: get all comments for a video
   const { videoId } = req.params;
@@ -83,7 +84,7 @@ const addComment = asyncHandler(async (req, res) => {
       video: videoId,
       owner: req.user?._id,
     });
-      return res.status(201).json(
+    return res.status(201).json(
       new ApiResponse(
         200,
         {
@@ -93,17 +94,63 @@ const addComment = asyncHandler(async (req, res) => {
       )
     );
   } catch (error) {
-     console.log("Error while creating comment", error);
+    console.log("Error while creating comment", error);
     throw new ApiError(400, "Comment not created");
   }
 });
 
 const updateComment = asyncHandler(async (req, res) => {
   // TODO: update a comment
+
+  const { commentId } = req.params;
+  const { content } = req.body;
+
+  if (!content?.trim()) {
+    throw new ApiError(404, "Comment should not be empty");
+  }
+
+  const comment = await Comment.findByIdAndUpdate(
+    commentId,
+    {
+      $set: {
+        content,
+      },
+    },
+    { new: true }
+  );
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        comment,
+      },
+      "Comment updated successfully"
+    )
+  );
 });
 
 const deleteComment = asyncHandler(async (req, res) => {
   // TODO: delete a comment
+
+  const { commentId } = req.params;
+
+  try {
+    const comment = await Comment.deleteOne({ _id: commentId });
+
+    res.status(200).json(
+      new ApiResponse(
+        200,
+        {
+          "Comment deleted": comment.deletedCount,
+        },
+        "Comment deleted succesfully"
+      )
+    );
+  } catch (error) {
+    console.log("Error occurred while deleting comment", error);
+    throw new ApiError(400, "Comment not deleted");
+  }
 });
 
 export { getVideoComments, addComment, updateComment, deleteComment };
--- Backend/src/controllers/healthcheck.controller.js
+++ Backend/src/controllers/healthcheck.controller.js
@@ -4,6 +4,25 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
   //TODO: build a healthCheck response that simply returns the OK status as json with a message
+  const DatabaseStatus =
+    mongoose.connection.readyState === 1
+      ? "Database Connected"
+      : "Database Disconnected";
+  const ServerUptime = `${process.uptime().toFixed(2)}seconds`;
+  const message = "Server is healthy";
+  if (!DatabaseStatus) {
+    message + " but database connection not established properly.";
+  }
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        DatabaseStatus,
+        ServerUptime,
+      },
+      message
+    )
+  );
 });
 
 export { healthcheck };
