c3a6f7872eeac352789cb23f8d3e74554447d439
Istekhar <istekhararkam25@example.com>
2026-06-25 20:31:57 +0530

    feat: update multer storage configuration to use temporary directory for file uploads

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -2,11 +2,13 @@
 
 // Step one is multer will upload the file in local server temporary, and next step  cloudinary will take those file from local server or storage and and upload in server.
 
+// https : it is used to encrypt the text,generally unreadable normally,can read in server can read in client but not in between.
+
 import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public");
+    cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
